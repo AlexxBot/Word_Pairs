01fc1f115a1466a48eac2b7b43b0eddbb0fffef7
Builder <builder@Builders-Mac-347.local>
2020-06-29 13:30:48 -0700

    commit desde mac

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Welcome to Flutter",
       theme: ThemeData(
-        primaryColor: Colors.red 
+        primaryColor: Colors.blue
         ),
       home: RandomWords(),
     );
